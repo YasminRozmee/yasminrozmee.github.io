0a4ddf23418fd94c10b896477e2c889f3ed3ed0b
Yasmin Binti Rozmee <yasminrozmee@Yasmins-MBP.wireless.newschool.edu>
2017-10-31 09:19:44 -0400

    upload

---
+++ Audience Participation /something.js
@@ -0,0 +1,28 @@
+var cash = 2000;
+
+update_wallet();
+
+$('.item').click(function() {
+	var item =$(this) .data('item');
+	var amount =$(this) .data('amount');
+
+	charge_card(amount, item);
+
+});
+
+function charge_card(amount, item) {
+
+	if (cash >= amount) {
+	cash = cash - amount; 
+	update_wallet();
+	$('.closet-items').append(item +', ');
+} 
+	else { 
+	alert('congrats. youre broke.');
+	
+}
+}
+
+function update_wallet() {
+	$('.cash').html(cash);
+}
