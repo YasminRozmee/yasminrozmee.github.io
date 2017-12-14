5423f471da20e0194f32d415016277df79221943
Yasmin Binti Rozmee <yasminrozmee@Yasmins-MBP.home>
2017-12-14 12:49:38 -0500

    changed final

--- FINAL/secondquiz.js
+++ FINAL/secondquiz.js
@@ -1,12 +1,15 @@
-var Quest = new Array(10);  
+var Quest = new Array(10);  //this sets up an array for all of the answers that are given 
 
 function populate() { 
-/
+// alert("function populate started"); 
+// this function gives each of the answers 0 points so if someone doesn't answer a question 
+// the whole thing will continue to work 
   for(var i=0; i<10; i++) { Quest[i]=0; } 
 } 
 
 function total() { 
- 
+// alert("function total started"); 
+// this function adds the number of points each answer is worth together 
   myScore=0; 
   for (var i=0; i<10; i++) { myScore=myScore+Quest[i]; } 
   analyzer(myScore); 
@@ -17,14 +20,14 @@ myContents = new Array();
   myContents[1] = "You Are a  "  + '<a href="CORINA.html" style="color: #a1ddd6; text-decoration: none;">Corina</a>'; 
   myContents[2] = "You Are a " + '<a href="THANH.html" style="color: #a1ddd6; text-decoration: none;">Thanh</a>';
   myContents[3] = "You Are a  "  + '<a href="MEGAN.html" style="color: #a1ddd6; text-decoration: none;">Megan</a>';  
-  myContents[4] = "You Are a. "   + '<a href="YASMIN.html" style="color: #a1ddd6; text-decoration: none;">Yasmin</a>';  
+  myContents[4] = "You Are a  "   + '<a href="YASMIN.html" style="color: #a1ddd6; text-decoration: none;">Megan</a>';  
   myContents[5] = "All of US"  + ""
 
 
 
 
 function analyzer (myScore) { 
-// score to determine the personality type.
+// this function uses the total calculated score to figure out which personality type they are 
   if (myScore>30)            { myContentsPtr = 4; } //6
     else { if (myScore > 30) { myContentsPtr = 4; } //5
     else { if(myScore>23)    { myContentsPtr = 3; } //4
@@ -40,12 +43,13 @@ function analyzer (myScore) {
 
 
 function myDisplay(myContents) { 
+
  
 document.getElementById("result").innerHTML = (myContents);
 } 
 
 function saver(q, points) { 
-// this function for each answer is worth into the array 
+
   q=q-1; 
   Quest[q]=points 
 } 
